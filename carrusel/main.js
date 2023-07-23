fec25e6930ed179d895842a6894c1bb5e00f048b
SilviaBec <89157289+SilviaBec@users.noreply.github.com>
2023-07-23 08:39:23 -0500

    Initial commit

---
+++ carrusel/main.js
@@ -0,0 +1,18 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin:10,
+    nav: true, 
+    items:1,
+    dots:true,
+    responsive:{
+        0:{
+            items: 1
+        },
+        600:{
+            items: 1
+        },
+        1000:{
+            items: 1
+        },
+    }   
+})
